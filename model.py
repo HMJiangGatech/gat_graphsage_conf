ce5d75f2b3ed34dd87df11e5ce8a71f28c60566e
ifif-S <xunyifu@gmail.com>
2019-04-07 00:32:40 -0400

    upload for vm2 cuda5

--- model.py
+++ model.py
@@ -107,7 +107,7 @@ def run_cora(device, opt):
     ##rig, wrg = sepRgtWrg(F.softmax(test_output,dim = 1).data.numpy().argmax(axis=1), labels[np.array( list(test))])
     ##plotGraphStrc(train + list(test[rig])+list(test[wrg]),torch.cat( (graphsage(train).data ,test_output.data[rig], test_output.data[wrg]), dim = 0),[0]*len(train)+[1]*len(rig)+[2]*len(wrg) , adj_lists, time = filetime, name = 'after self-train model miscls' )
     return loss_Data+loss_DataSelf, scores, test_output, labels[np.array(batch_nodes)], labels[test], graphsage, test, filetime
-
+"""
 def run_ppi(device, opt):
 
     filetime = datetime.datetime.now()
@@ -183,7 +183,7 @@ def run_ppi(device, opt):
     return loss_Data+loss_DataSelf, scores, test_output, labels[np.array(batch_nodes)], labels[test], graphsage, test, filetime
 
 
-  
+"""
     
 
 def selfTrain(graphsage, labels, train, test, val, confList, adj_lists, filetime, features, temperature = 1, update_interval = 50, maxiter = 500, tol = 0.5, batch_size = 200  ): 
