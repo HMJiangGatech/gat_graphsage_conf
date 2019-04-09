3b5bd6ecc294efe0bc0ee1ea4d5cde6f121c2326
ifif-S <xunyifu@gmail.com>
2019-04-09 14:34:34 -0400

    upload for vm2 cuda5

--- opts.py
+++ opts.py
@@ -16,7 +16,7 @@ class BaseOptions():
 
     def initialize(self):
         self.parser.add_argument('--dataroot', type=str, default='./data', help='path')
-        self.parser.add_argument('--dataset', type=str, default='pubmed', help='[cora | citeseer | pubmed| ppi| reddit]')
+        self.parser.add_argument('--dataset', type=str, default='ppi', help='[cora | citeseer | pubmed| ppi| reddit]')
         self.parser.add_argument('--num_features', type=int, default=1433, help='number of features')
         self.parser.add_argument('--num_hidden', type=int, default=180, help='number of hidden layer dimension')
         self.parser.add_argument('--res_path',type=str, default= 'result/result_para',help = 'the path to save result and parameter')
